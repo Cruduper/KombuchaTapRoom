33c6b619342521cbc3414db4c4156aa92c28a82f
Eric Crudup <briefcasemanx@gmail.com>
2022-04-22 11:45:41 -0700

    adds render branching

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import KegList from './KegList';
+import KegDetail from './KegDetail';
 
-class TicketControl extends React.Component {
+
+class KegControl extends React.Component {
 
   constructor(props) {
     super(props);
@@ -13,24 +16,59 @@ class TicketControl extends React.Component {
 
   handleClick = () => {
     const {currentProductVisible, selectedKeg} = this.state;
-
+    const {setState} = this;
     if (currentProductVisible === true){
       if (selectedKeg === null) {
-        <KegList />
+        setState({currentProductVisible: false});
       } else {
-        <KegDetail />
+        setState({selectedKeg: null});
       }
 
     } else {
-      <newKegForm />
+      setState({ 
+        currentProductVisible: true,
+        selectedKeg: null 
+      });
     }
   }
 
+    handleAddingNewKeg = (newKeg) => {
+      const {mainKegList} = this.state;
+      const {setState} = this;
+
+      const newKegList = mainKegList.concat(newKeg);
+      setState({ mainKegList: newKegList});
+    }
+
   render() {
+    const {currentProductVisible, selectedKeg, mainKegList} = this.state;
+    const {handleClick, handleAddingNewKeg} = this;
+    let currentlyVisibleState = null;
+    let buttonText = null;
+
+    if (currentProductVisible === true){
+        if (selectedKeg === null) {
+          currentlyVisibleState = <KegList kegList={mainKegList}/>;
+          buttonText = "Add Keg";
+        } else {
+          currentlyVisibleState = <KegDetail />;
+          buttonText = "Return To Keg List";
+        }
+
+      } else {
+        currentlyVisibleState = <newKegForm onAddingNewKeg={handleAddingNewKeg}/>;
+        buttonText = "Return To Keg List";
+      }
 
 
     return (
-      <></>
+      
+      <>
+        {currentlyVisibleState}
+        <button onClick={handleClick}>{buttonText}</button>
+      </>
     );
   }
 }
+
+export default KegControl;
