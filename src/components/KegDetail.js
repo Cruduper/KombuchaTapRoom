6c12558f64f2f9766f691d2976ada9375f9ad6ba
Eric Crudup <briefcasemanx@gmail.com>
2022-04-22 16:39:05 -0700

    WIP: getting screen to visually update and display pintsLeft when state changes

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -6,14 +6,14 @@ function KegDetail(props){
 const { keg, onDeletingKeg, onDecrementingPints } = props;
 
   return (
-    <React.Fragment>
+    <React.Fragment >
       <h3>Name: {keg.name}</h3>
       <p>Brand: {keg.brand}</p>
       <p>Price: {keg.price}</p>
       <p>Alcohol % (abv): {keg.alcPercent}</p>
       <p>Pints Left: {keg.pintsLeft}</p>
       <button onClick={()=> onDeletingKeg(keg.id) }>Delete Keg</button>
-      <button onClick={()=> onDecrementingPints(keg.id) }>Pour A Pint</button>
+      <button onClick={()=> onDecrementingPints(keg.id) } >Pour A Pint</button>
       <hr/>
     </React.Fragment>
   );
